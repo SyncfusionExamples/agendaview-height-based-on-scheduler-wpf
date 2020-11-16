1da96b93aa268a57661d4085e021eb711d62e058
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-16 17:45:40 +0530

    WPF-55632 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.SizeChanged += AssociatedObject_SizeChanged;
+        }
+        private void AssociatedObject_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.AssociatedObject.MonthViewSettings.AgendaViewHeight = scheduler.Height * 0.6;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SizeChanged -= AssociatedObject_SizeChanged;
+            this.scheduler = null;
+        }
+    }
+}
+        
+    
+
+    
+
